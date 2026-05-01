e81e006773205dc75e3677853105a939db0422f4
Izzy Bledsoe <ibledsoe1@collin.edu>
2026-04-30 22:12:50 -0500

    2nd attmpt trying to fix init files for actions tests

--- campus_store/order_service/src/__init__.py
+++ campus_store/order_service/src/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from db.main import init_db
-from orders.routes import order_router
+from src.db.main import init_db
+from src.orders.routes import order_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
